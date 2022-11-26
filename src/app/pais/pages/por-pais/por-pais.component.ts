23cc66597fe27b9e2f345038d2c6eb94c99192c3
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-26 02:38:03 -0300

    import form module y manejo de errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class PorPaisComponent implements OnInit {
+  constructor(private paisService: PaisService) {}
+  termino: string = '';
+  hayError: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  buscar() {
+    this.hayError = false;
+    this.paisService.buscarPais(this.termino).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        this.hayError = true;
+      }
+    );
   }
-
 }
