b5ba882fe97adb8634250567152cbe68e53aa9d6
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-29 16:48:29 -0300

    extraer el id y mandarlo al metodo obtener pais por alphaCode

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private paisService: PaisService) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ countryId }) => {
+      console.log(countryId);
+      this.paisService.getPaisPorAlphaCode(countryId).subscribe(pais => {
+        console.log(pais);
+        
+      })
+    });
   }
-
 }
