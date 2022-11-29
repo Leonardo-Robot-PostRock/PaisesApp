27f38046ae9d9b7a94359e977da4e1f0c6d66777
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-29 16:49:25 -0300

    metodo obtener pais por alpha code

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -15,9 +15,15 @@ export class PaisService {
     const url = `${this.apiUrl}/name/${termino}`;
     return this.http.get<Country[]>(url);
   }
-  
+
   buscarCapital(termino: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${termino}`;
     return this.http.get<Country[]>(url);
+  }
+  getPaisPorAlphaCode(id: string): Observable<Country> {
+    const url = `${this.apiUrl}/alpha/${id}`;
+    console.log(url);
+     
+    return this.http.get<Country>(url);
   }
 }
