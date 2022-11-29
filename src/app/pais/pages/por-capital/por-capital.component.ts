818e2f9e0d633153c9837b53e0a7460de23a5029
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-29 01:14:08 -0300

    por capital metodos de buscar

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+  constructor(private paisService: PaisService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+    this.paisService.buscarCapital(this.termino).subscribe(
+      (paises) => {
+        this.paises = paises;
+      },
+      (error) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
-
 }
