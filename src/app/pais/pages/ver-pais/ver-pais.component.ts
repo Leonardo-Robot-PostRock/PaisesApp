ccdd22c83af10d24985deea5193b906d6d05ceaf
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-29 21:55:38 -0300

    variable para pintar componente operador rxjs para obtener id

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -8,15 +10,24 @@ import { PaisService } from '../../services/pais.service';
   styles: []
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country;
   constructor(private activatedRoute: ActivatedRoute, private paisService: PaisService) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({ countryId }) => {
-      console.log(countryId);
-      this.paisService.getPaisPorAlphaCode(countryId).subscribe(pais => {
-        console.log(pais);
-        
-      })
-    });
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ countryId }) => this.paisService.getPaisPorAlphaCode(countryId)),
+        tap(console.log)
+      )
+      .subscribe((pais) => {
+        this.pais = pais;
+      });
+    // this.activatedRoute.params.subscribe(({ countryId }) => {
+    //   console.log(countryId);
+    //   this.paisService.getPaisPorAlphaCode(countryId).subscribe(pais => {
+    //     console.log(pais);
+
+    //   })
+    // });
   }
 }
