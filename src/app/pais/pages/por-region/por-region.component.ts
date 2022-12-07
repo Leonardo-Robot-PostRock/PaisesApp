d7d05a621b97d1159cb24147fcda0098c98f3236
Leonardo Puebla <leonardo38361@gmail.com>
2022-12-07 20:13:15 -0300

    clases css metodo activar region con subscribe a peticion

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -7,18 +9,45 @@ import { Component, OnInit } from '@angular/core';
     `
       button {
         margin-right: 5px;
+        margin-bottom: 5px;
       }
     `
   ]
 })
 export class PorRegionComponent implements OnInit {
-  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regiones: string[] = [
+    'EU',
+    'EFTA',
+    'CARICOM',
+    'PA',
+    'AU',
+    'USAN',
+    'EEU',
+    'AL',
+    'ASEAN',
+    'CAIS',
+    'CEFTA',
+    'NAFTA',
+    'SAARC'
+  ];
+  paises: Country[] = [];
   regionActiva: string = '';
 
-  activarRegion(region:string){
-    this.regionActiva = region;
+  constructor(private paisService: PaisService) {}
+  getClaseCSS(region: string) {
+    return region === this.regionActiva ? 'btn btn-primary' : 'btn btn-outline-primary';
   }
-  constructor() {}
+
+  activarRegion(region: string) {
+    if (region === this.regionActiva) {
+      return;
+    }
+    this.regionActiva = region;
+    this.paises = [];
+    this.paisService.buscarRegion(region).subscribe((paises) => {
+      this.paises = paises;
+    });
+  }
 
   ngOnInit(): void {}
 }
